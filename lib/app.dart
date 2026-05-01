88c754c2b1e63e9ba1a870ab2eb8e168964e449c
Scott Milliorn <scottmilliorn@gmail.com>
2026-04-30 19:52:29 -0700

    refactor(app): change UvAlertApp from ConsumerWidget to StatelessWidget

--- lib/app.dart
+++ lib/app.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class UvAlertApp extends ConsumerWidget {
+class UvAlertApp extends StatelessWidget {
   const UvAlertApp({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  Widget build(BuildContext context) {
     return MaterialApp(
       title: 'UV Alert',
       theme: ThemeData(
